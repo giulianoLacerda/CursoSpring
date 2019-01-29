4bf32c85427569a2f7240edf7fca135deda76126
Giuliano L. Manso <giuliano.lacerda@gmail.com>
2019-01-29 00:47:20 -0200

    Personalizando o retorno da validação.

--- src/main/java/com/giuliano/curso/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/giuliano/curso/resources/exception/ResourceExceptionHandler.java
@@ -4,9 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.giuliano.curso.resources.ValidationError;
 import com.giuliano.curso.services.exceptions.DataIntegrityException;
 import com.giuliano.curso.services.exceptions.ObjectNotFoundException;
 
@@ -28,13 +31,28 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err); // Retorna a mensagem com status 404 contida em seu corpo os detalhes do erro.
 	}
 	
-	// Trata exceção quando o objeto da requisição não é encontrado.
+	// Trata exceção quando requisição não possui todos os campos necessários.
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StandardError> objectNotFound(DataIntegrityException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		
 		// Cria o objeto de erro padrão e envia este objeto como resposta usando o código
 		// 400 BAD REQUEST.
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); // Retorna a mensagem com status 404 contida em seu corpo os detalhes do erro.
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		// Cria o objeto de erro padrão e envia este objeto como resposta usando o código
+		// 400 BAD REQUEST.
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		// Para cada campo de erro na mensagem de erro, converte para ValidationError.
+		// Ou seja, teremos uma lista de erros de validação.
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); // Retorna a mensagem com status 404 contida em seu corpo os detalhes do erro.
 	}
 	
