e0e8798a0a3aa93a632f3568491f0f4b45206fc6
Giuliano L. Manso <giuliano.lacerda@gmail.com>
2019-01-31 23:45:37 -0200

    Validação customizada de CPF ou CNPJ na inserção de Cliente.

--- src/main/java/com/giuliano/curso/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/giuliano/curso/services/validation/ClienteInsertValidator.java
@@ -6,8 +6,10 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.giuliano.curso.domain.enums.TipoCliente;
 import com.giuliano.curso.dto.ClienteNewDTO;
 import com.giuliano.curso.resources.FieldMessage;
+import com.giuliano.curso.services.validation.utils.CpfCnpj;
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -16,7 +18,15 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		
+		// Verifica se é pessoa física e se o cpf é válido.
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !CpfCnpj.isValidCpf(objDto.getCpfOuCnpj())){
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
 		
+		// Verifica se é pessoa jurídica e se o cnpj é válido.
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !CpfCnpj.isValidCnpj(objDto.getCpfOuCnpj())){
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
 		
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
